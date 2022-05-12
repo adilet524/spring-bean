3738b5e1e8d245bf36c9d3989cba55218bafaf46
adilet <zhumakadyrovadilet524@gmail.com>
2022-05-12 14:48:37 +0600

    Homework_9

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -5,10 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        System.out.println(cat2.equals(cat));
+
     }
 }
